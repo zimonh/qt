37ed4c5fe0cc7825196eeb0c1fe239105baaa1a1
ZIMONH <zh@zimonh.at>
2018-06-14 22:38:21 +0200

    protected grabber

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php
-/********Set develop modus********/
-$d = '';
-//$d = '?'.date("Y.m.d.h.m.s");
+	//block the grabber script from cross origin
+	session_start(['cookie_lifetime' => 600,]);
+	$_SESSION["origin"] = true;
 
-/********uses the .htacces file to redirect and set the page variable********/
-$page = (isset($_GET['p']) ? $_GET['p'] : null);
+	//developer mode
+	$d = '';
+	//$d = '?'.date("Y.m.d.h.m.s");
 
-//triggerd with ?! at the end of url
-if(isset($_GET['!'])){$save_mode = true;}else{$save_mode = false;};
+	$page = (isset($_GET['p']) ? $_GET['p'] : null); //uses .htacces to redirect and set page variable
 
-//triggerd with ?$ at the end of url
-if(isset($_GET['@'])){$clean_mode = true;}else{$clean_mode = false;};
+	if(isset($_GET['!'])){$save_mode = true;}else{$save_mode = false;}; //triggerd with ?! at the end of url
+	if(isset($_GET['@'])){$clean_mode = true;}else{$clean_mode = false;}; //triggerd with ?$ at the end of url
 
-preg_match("/^[A-Za-z0-9,' .\-]+$/i", $page);
+	preg_match("/^[A-Za-z0-9,' .\-]+$/i", $page);
 
-/*******Force that the page is fresh like a bleached asshole*****/
-header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
-header("Pragma: no-cache"); // HTTP 1.0.
-header("Expires: 0"); // Proxies.
+	/*******Force that the page is fresh like a bleached asshole*****/
+	header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+	header("Pragma: no-cache"); // HTTP 1.0.
+	header("Expires: 0"); // Proxies.
 
-include 'r/connect/select.php';
-include 'r/connect/insert.php';
-include 'r/php/translation.php';
-include 'r/php/top.php';
-include 'r/php/select.php';
-if($clean_mode){echo "</body>\n</html>";}else{include 'r/php/bottom.php';}
+	include 'r/connect/select.php';
+	include 'r/connect/insert.php';
+	include 'r/php/translation.php';
+	include 'r/php/top.php';
+	include 'r/php/select.php';
+	if($clean_mode){echo "</body>\n</html>";}else{include 'r/php/bottom.php';}
 
 
 
--- r/ajax/grabber.php
+++ r/ajax/grabber.php
@@ -1,73 +1,72 @@
 <?php
+//block from cross origin works with index where it is initiated
+//and uses credentials from fetch(url,{credentials: 'same-origin'})
+session_start();
+if($_SESSION["origin"]){
+
 	$id = $_GET["id"];
 	$href = $_GET["href"];
-	$adjust = $_GET["adjust"];
-/* gets the data from a URL */
-function get_data($href) {
-	$ch = curl_init();
-	$timeout = 5;
-	curl_setopt($ch, CURLOPT_URL, $href);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-	$data = curl_exec($ch);
-	curl_close($ch);
-	return $data;
+		$adjust = $_GET["adjust"];
+	/* gets the data from a URL */
+	function get_data($href) {
+		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch, CURLOPT_URL, $href);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+	$stringy = get_data($href);
+
+	$dom = new DOMDocument();
+	libxml_use_internal_errors(true);
+	$dom->loadHTML($stringy);//put your complete source html string here
+	libxml_use_internal_errors(false);
+
+
+	$domx = new DOMXPath($dom);
+	$items = $domx->query("//img[@onerror]");
+
+	foreach($items as $item){$item->removeAttribute("onerror");}
+
+	foreach(iterator_to_array($dom->getElementsByTagName('script')) as $node){
+	$node->parentNode->removeChild($node);};
+	foreach(iterator_to_array($dom->getElementsByTagName('input' )) as $node){
+	    $node->parentNode->removeChild($node);};
+	foreach(iterator_to_array($dom->getElementsByTagName('header')) as $node){
+	    $node->parentNode->removeChild($node);};
+	foreach(iterator_to_array($dom->getElementsByTagName('meta'  )) as $node){
+	    $node->parentNode->removeChild($node);};
+	foreach(iterator_to_array($dom->getElementsByTagName('link'  )) as $node){
+	    $node->parentNode->removeChild($node);};
+
+	if(isset($id) && $id !== ''){
+	$elm=$dom->getElementById($id);}else{
+		$elm=$dom;
+	}//this will output DOMElement Object
+
+	$tag = $elm->tagName;
+	$cnt = $elm->nodeValue;
+	//replace src="/ with src="$href/
+
+
+	$str  = $dom->saveHtml($elm);
+	if($adjust ==='yes'){
+	if(parse_url($href)["host"] === "www.youtube.com"){
+
+		$str = str_replace('href="/','href="https://'.parse_url($href)["host"].'/',$str);
+
+	}else{
+		$str = str_replace('src="/','src="'.$href.'/',$str);
+		$str = str_replace('href="/','href="'.$href.'/',$str);
+	}
+	}
+
+	if($href === 'http://www.mrlovenstein.com'){$str = str_replace('.png','_rollover.png',$str);}
+
+	echo $str;
+
 }
-$stringy = get_data($href);
-
-$dom = new DOMDocument();
-libxml_use_internal_errors(true);
-$dom->loadHTML($stringy);//put your complete source html string here
-libxml_use_internal_errors(false);
-
-
-$domx = new DOMXPath($dom);
-$items = $domx->query("//img[@onerror]");
-
-foreach($items as $item){$item->removeAttribute("onerror");}
-
-
-
-foreach(iterator_to_array($dom->getElementsByTagName('script')) as $node){
-$node->parentNode->removeChild($node);};
-
-foreach(iterator_to_array($dom->getElementsByTagName('input' )) as $node){
-    $node->parentNode->removeChild($node);};
-
-foreach(iterator_to_array($dom->getElementsByTagName('header')) as $node){
-    $node->parentNode->removeChild($node);};
-
-foreach(iterator_to_array($dom->getElementsByTagName('meta'  )) as $node){
-    $node->parentNode->removeChild($node);};
-
-foreach(iterator_to_array($dom->getElementsByTagName('link'  )) as $node){
-    $node->parentNode->removeChild($node);};
-
-if(isset($id) && $id !== ''){
-$elm=$dom->getElementById($id);}else{
-	$elm=$dom;
-}//this will output DOMElement Object
-
-$tag = $elm->tagName;
-$cnt = $elm->nodeValue;
-//replace src="/ with src="$href/
-
-
-$str  = $dom->saveHtml($elm);
-if($adjust ==='yes'){
-if(parse_url($href)["host"] === "www.youtube.com"){
-
-	$str = str_replace('href="/','href="https://'.parse_url($href)["host"].'/',$str);
-
-}else{
-	$str = str_replace('src="/','src="'.$href.'/',$str);
-	$str = str_replace('href="/','href="'.$href.'/',$str);
-}
-}
-
-if($href === 'http://www.mrlovenstein.com'){$str = str_replace('.png','_rollover.png',$str);}
-
-echo $str;
-
-
 ?>
